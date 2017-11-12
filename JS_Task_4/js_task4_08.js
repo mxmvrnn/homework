6966306acb59a6de249c0551b213038a808ea85f
blackamlet <paskalzlo@bk.ru>
2017-11-12 05:34:26 +0300

    разгребаю бардак

--- JS_Task_4/js_task4_08.js
+++
@@ -1,6 +0,0 @@
-/* Написать функцию, которая принимает строку и возвращает перевернутую строку */
-
-function strReverse(str){
-    console.log(str.split('').reverse().join(''));
-};
-strReverse('ьшедйорпеныт');

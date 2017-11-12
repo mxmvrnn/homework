433be765fb77caa836c9ff270020449648cc88dd
blackamlet <paskalzlo@bk.ru>
2017-11-12 05:35:41 +0300

    разгребаю бардак

--- JS_Task_4/js_task4_02.js
+++
@@ -1,9 +0,0 @@
-/* Написать функцию, которая принимает строку
-и возвращает эту же строку, на с заглавным первым символом */
-
-
-function strUp(str){
-    return str[0].toUpperCase() + str.slice(1);
-};
-
-console.log(strUp('abcd'));

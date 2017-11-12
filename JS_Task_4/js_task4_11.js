98701886ab0556e976c3496f0bdbb087935970c5
blackamlet <paskalzlo@bk.ru>
2017-11-12 05:33:33 +0300

    разгребаю бардак

--- JS_Task_4/js_task4_11.js
+++
@@ -1,7 +0,0 @@
-/* Написать функцию, которая возвращает случайное число в диапазоне от 0 до 100 */
-
-function getRandom() {
-  return Math.random() * 100;
-};
-console.log(getRandom());
-
--- JS_Task_4/js_task4_12.js
+++
@@ -1,7 +0,0 @@
-/* Написать функцию, которая возвращает целое случайное число в диапазоне от min до max */
-
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  };
-
-  console.log(getRandomArbitrary(50,100));

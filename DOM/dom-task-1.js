10a50b53d61f903b6f43631cf3215e2e58dd0e81
blackamlet <paskalzlo@bk.ru>
2017-11-30 20:07:08 +0300

    task_1

--- DOM/dom-task-1.js
+++ DOM/dom-task-1.js
@@ -1,10 +1,7 @@
 function prepend(container1, newElement1){ 
     var a = document.createElement(newElement1);
-        // b = document.getElementById(container1);
-
-        a . innerHTML = 'Привет, мир!'; 
-
-        container1.insertBefore(a , container1.children[2]);
+        a.innerHTML = 'Привет, мир!';
+    container1.insertBefore(a , container1.children[2]);
     
     };     
     prepend(divv,"p");

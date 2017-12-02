e008fa8f8c3f0f0711627d315523c96ced979123
blackamlet <paskalzlo@bk.ru>
2017-12-02 13:00:15 +0300

    фикс третий таски

--- DOM/dom-task-3.js
+++ DOM/dom-task-3.js
@@ -5,12 +5,27 @@ function removeChildren(elem) {
         if (elemChildren[i-1].nodeType == 3) {
             elem.removeChild(elemChildren[i-1]);
                     
-        } else {
-            var elem = elemChildren[i];
-            removeChildren(elemChildren[i-1]);            
-        }
+        } else{
+            // elem = elemChildren[i-1];
+            removeChildren(elemChildren[i-1]);  
+        }   
     }
     //  elem.childNodes[1].innerHTML = '';
 };
 removeChildren(body);
+
+/* function removeChildren(elem) {
+    var elemChildren = elem.childNodes;
+    var length = elemChildren.length;
+    for (var i = length; i > 0; i-- ){            
+        if (elemChildren[i-1].nodeType == 3) {
+            elem.removeChild(elemChildren[i-1]);
+    } else {
+            //var elem = elemChildren[i];
+            removeChildren(elemChildren[i-1]);
+        }
+    }
+    //  elem.childNodes[1].innerHTML = '';
+};
+removeChildren(body);  */ 
 
--- DOM/dom-task-4.js
+++ DOM/dom-task-4.js
@@ -1,13 +1,13 @@
 function removeChildren(elem) {
     var elemChildren = elem.childNodes;
     var length = elemChildren.length;
-    for (var i = 0; i < length; i++ ){            
+    for (var i = length; i > 0; i-- ){            
         if (elemChildren[i-1].nodeType == 3) {
             elem.removeChild(elemChildren[i-1]);
                     
         } else {
             var elem = elemChildren[i];
-            removeChildren(elemChildren[i-1]);            
+            removeChildren(elem);            
         }
     }
     //  elem.childNodes[1].innerHTML = '';

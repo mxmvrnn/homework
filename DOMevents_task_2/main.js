80ee6957677e98dcd177407a295cee530444d4d5
blackamlet <paskalzlo@bk.ru>
2017-12-03 21:28:10 +0300

    получилось добавить Drag'n'Drop к дивам

--- DOMevents_task_2/main.js
+++ DOMevents_task_2/main.js
@@ -1,4 +1,5 @@
-function randomColor(){ 
+// случайный цвет
+function randomColor(){
     var randColor='#'+rand()+rand()+rand();
     return randColor;
 };
@@ -9,37 +10,66 @@ function rand(){
     return (""+c).length==1?'0'+c:c;
 };
 
-
+// случайный размер
 function randSize() {
     return Math.random() * 700 + "px";
 }
-
+// случайная позиция
 function randPosition() {
     return Math.random() * 1000 + "px";
 }
-
+// функция создания дива, придания ему свойств 
 (function () {
 
     var MyButton = document.getElementById('main-button');
+    var divContainer = document.getElementById('div-container');
+
     MyButton.onclick = function(){
         // document.body.removeChild(myDiv);
         var myDiv = document.createElement('div');
-        myDiv.innerHTML = 'Привет, мир!';
-        
-    
-    
-        document.body.appendChild(myDiv)
+        myDiv.className = 'dnd-div';
+        myDiv.innerHTML = 'KEK MAKEK';
+        divContainer.appendChild(myDiv);
+
         myDiv.style.position = 'absolute';
         myDiv.style.background = randomColor();
         myDiv.style.height = randSize();
         myDiv.style.width = randSize();
         myDiv.style.top = randPosition();
         myDiv.style.left = randPosition();
-    
-    
-        /* document.body.removeChild(myDiv); */
     }
-}()) 
+}())
+
+
+var dragObject = {};
+var divContainer = document.getElementById('div-container');
+var dndDiv = document.getElementsByClassName('dnd-div');
+
+
+divContainer.onmousedown = function(e){
+    /* if (e.which != 1) { 
+        return; 
+    } */
+    var elem = e.target.closest('.dnd-div');
+    elem.style.position = 'absolute';
+    elem.style.zIndex = 1000;
+    function moveAt(e) {
+        elem.style.left = e.pageX - elem.offsetWidth / 2 + 'px';
+        elem.style.top = e.pageY - elem.offsetHeight / 2 + 'px';
+    }
+
+    divContainer.onmousemove = function(e) {
+        moveAt(e);
+    }
+
+    elem.onmouseup = function() {
+        divContainer.onmousemove = null;
+        ball.onmouseup = null;
+        elem.style.zIndex = 1;
+      }
+
+}
+
 
 
 
